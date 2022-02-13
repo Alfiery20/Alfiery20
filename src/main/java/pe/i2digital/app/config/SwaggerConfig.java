ec6d4104a56f08d88b6b6beaf8e1b39451b58f45
Alfiery20 <alfieryfurlong18@gmail.com>
2022-02-12 19:49:31 -0500

    Commit antes de formateo

--- src/main/java/pe/i2digital/app/config/SwaggerConfig.java
+++ src/main/java/pe/i2digital/app/config/SwaggerConfig.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pe.i2digital.config;
+package pe.i2digital.app.config;
 
 import java.util.ArrayList;
 import java.util.Set;
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
@@ -23,30 +24,34 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * @author alfie
  */
 @Configuration
-@EnableSwagger2
+//@EnableSwagger2
+@EnableOpenApi
 public class SwaggerConfig {
-
     @Bean
-    public Docket apiV1() {
+    public Docket apiV1(){
         return new Docket(DocumentationType.OAS_30)
                 .groupName("appat-api-1.0")
-                .apiInfo(getApiInfo())
+                //.apiInfo(getApiV1Info())
                 .produces(Set.of(MediaType.APPLICATION_JSON_VALUE))
                 //.consumes(Set.of(MediaType.APPLICATION_JSON_VALUE))
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("p2.i2digital.app"))
+                .apis(RequestHandlerSelectors.basePackage("pe.i2digital.app"))
+                //.paths(PathSelectors.any())
                 .paths(PathSelectors.ant("/api/v1/**"))
                 .build();
     }
-
-    public ApiInfo getApiInfo(){
-        return new ApiInfo("Documentacion del REST API - APP-AT",
-                "Documentacion del API REST de los recursos del backend de la plataforma APP-AT",
+    public ApiInfo getApiV1Info(){
+        return new ApiInfo(
+                "Documentacion del REST Api - APP-AT",
+                "Documentacion del API Rest de los recursos del backend de la plataforma APP-AT",
                 "1.0",
                 "urn:tos",
-                new Contact("Omar Saavedra Salazar", "i2digital.pe", "osaavedra@unprg.edu.pe"),
-                "Apache 2.0",
-                "http://www.apache.org/licenses/LICENSE-2.0",
-                new ArrayList<>());
+                new Contact("Omar Saavedra Salazar", "i2digital.pe", "osaavedras@unprg.edu.pe"),
+                "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<>()
+        );
     }
+
+
+
 }
--- src/main/java/pe/i2digital/app/models/request/CuentaContableRequest.java
+++ src/main/java/pe/i2digital/app/models/request/CuentaContableRequest.java
@@ -1,6 +1,9 @@
 package pe.i2digital.app.models.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,6 +11,7 @@ import lombok.NoArgsConstructor;
 import pe.i2digital.app.models.entity.CuentaContable;
 import pe.i2digital.app.models.udt.DestinoCompraUDT;
 
+@ApiModel(description = "Estructura de la cuenta contable a IUD")
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -17,5 +21,6 @@ public class CuentaContableRequest {
     private CuentaContable oCuentaContable;
     @JsonProperty("aDestinoCompra")
     private DestinoCompraUDT[] aDestinoCompra;
+    @ApiModelProperty(notes = "Indicador de que se modifica los destinos")
     private Boolean updateD;
 }
